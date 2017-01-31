d6313152de6555f409e697f742b64e087d5ba45e
Preston <prestondaily1@gmail.com>
2017-01-31 09:01:10 -0600

    iub
    iub

---
+++ __SHELL5.java
@@ -0,0 +1,10 @@
+
+public class __SHELL5 extends bluej.runtime.Shell {
+public static void run() throws Throwable {
+final bluej.runtime.BJMap __bluej_runtime_scope = getScope("C:\\Users\\MVR3\\Documents\\GitHub\\CSA-Game");
+final game Perstone = (game)__bluej_runtime_scope.get("Perstone");
+
+
+Perstone.main();
+
+}}
--- game.java
+++ game.java
@@ -9,55 +9,46 @@ import javax.swing.JFrame;
 
 public class game
 {
-    public static void main(){
-        Scanner user_input = new Scanner(System.in);
-        
-        
-        
+    public void wait(int time){
         try {
-            Thread.sleep(750);
+            Thread.sleep(time);
         } 
         catch (InterruptedException ex) {
            
         }
+    }
+    public  void main(){
+        Scanner user_input = new Scanner(System.in);
         
-        
-        
+        wait(750);
         
         System.out.println("Welcome, please wait while game is configuring.");
         
-        
-        
-        
-        try {
-            Thread.sleep(3000); 
-     
-            //new page break-----------------------
-            for (int i = 0; i <= 30; i++){
-               System.out.println("\n");
-            }    
-            //new page break-----------------------
+        wait(3000);
+        //new page break-----------------------
+        for (int i = 0; i <= 40; i++){
+            System.out.println("\n");
+        }    
+        //new page break-----------------------
             
-            System.out.println("Loading...");
-            System.out.println("");
-            Thread.sleep(2000); 
-            System.out.println("3");
-            Thread.sleep(1000); 
-            System.out.println("2");
-            Thread.sleep(1000); 
-            System.out.println("1");
-            Thread.sleep(1000); 
+        System.out.println("Loading...");
+        System.out.println("");
+        wait(2000); 
+        System.out.println("3");
+        wait(1000); 
+        System.out.println("2");
+        wait(1000);  
+        System.out.println("1");
+        wait(1000);  
+        System.out.println("(Gotta Blast!)");
+        wait(1000); 
             
-            //new page break-----------------------
-            for (int i = 0; i <= 30; i++){
-               System.out.println("\n");
-            }    
-            //new page break-----------------------
-            
-        } 
-        catch (InterruptedException ex) {
-            
-        }
+        //new page break-----------------------
+        for (int i = 0; i <= 40; i++){
+           System.out.println("\n");
+        }    
+        //new page break-----------------------
+
         
         System.out.println("\n");
         System.out.println("=======================================================");
@@ -70,29 +61,47 @@ public class game
         System.out.println("#######################################################");
         System.out.println("=======================================================");
         System.out.println("\n");
+        wait(3000);
         System.out.println("-------------------------------------------------------");
-        System.out.println("The commands that you can call at anytime include: ");
-        System.out.println("-    '/health'");
-        System.out.println("-    '/armour'");
-        System.out.println("-    '/inventory'");
+        System.out.println("To check your stats at any time, use the following command.");
+        System.out.println("-    '/stats'");
+        
         System.out.println("-------------------------------------------------------");
         System.out.println("");
         System.out.println("\n");
         
         
         
-        System.out.println("Type your name to continue: ");
+        System.out.println("Type your name: ");
         String user_name = user_input.next();
+        System.out.println("");
         
-        try {
-            System.out.println("Storing...");
-            Thread.sleep(1500);
-        } 
-        catch (InterruptedException ex) {
-            
-        }
         
-        for (int i = 0; i <= 25; i++){
+        System.out.println("Storing...");
+        wait(1500);
+        
+        System.out.println("Your name is " + user_name);
+        
+        
+        System.out.println("");
+        System.out.println("");
+        
+        System.out.println("Type your gender: ");
+        String user_gender = user_input.next();
+        System.out.println("");
+        
+        
+        System.out.println("Storing...");
+        wait(1500);
+        
+        System.out.println("Your gender is " + user_gender);
+        
+        wait(1500);
+        
+        
+        
+        
+        for (int i = 0; i <= 35; i++){
             System.out.println("\n");
         }    
         
@@ -101,29 +110,101 @@ public class game
         System.out.println("your thoughts, you panic in");
         System.out.println("the realization that you cannot");
         System.out.println("remember the past.");
+        wait(1000);
         System.out.println("Finding yourself in a cold,");
         System.out.println("dark, and rather damp corridor.");
         System.out.println("The only visible light, permiates from");
         System.out.println("a door several paces away.");
         System.out.println("You notice a crate next to you.");
         System.out.println("It is emitting a strange sound.");
+        wait(1500);
         System.out.println("");
         System.out.println("");
         System.out.println("What do you do?");
+        
+        
+        
+        
+        
+        // STAT CHECK
+        
+        String stat_check = user_input.next();
+       
+        String lowerCheck = stat_check.toLowerCase();  
+        
+        
+        
+        // Health check
+        
+        String[] healthCheck = {"/health"};
+        for (String s: healthCheck)
+        {
+            if (s.toLowerCase().contains(lowerCheck))
+            {
+                System.out.println("");
+                System.out.println("Your health: 100/100");
+            }
+        }
+        
+        
+        
+        // Armour check
+        
+        String[] armourCheck = {"/armour"};
+        for (String s: healthCheck)
+        {
+            if (s.toLowerCase().contains(lowerCheck))
+            {
+                System.out.println("");
+                System.out.println("Your Armour rating: 10/10");
+            }
+        }
+        
+        
+        
+        // Inventory check
+        
+        String[] inventory = {"'Pencil'", "'necklace'"};
+        
+        String[] inventoryCheck = {"/inventory"};
+        for (String s: healthCheck)
+        {
+            if (s.toLowerCase().contains(lowerCheck))
+            {
+                System.out.println("");
+                System.out.println("Your inventory: " + inventory);
+            }
+        }
+        
+        
+        // END OF STAT CHECK SITUATION
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         String first_choice = user_input.next();
-        
-        
+       
         String lowerFirst = first_choice.toLowerCase();  
         
-        ArrayList applicable = new ArrayList()
-        
-        
-        for (Arr applicable);
-        applicable.add("walk");
-            if (applicable.toLowerCase().contains(lowerFirst))
+        String[] first_answer = {"hallway", "walk"};
+        for (String s: first_answer)
+        {
+            if (s.toLowerCase().contains(lowerFirst))
             {
                 System.out.println("");
                 System.out.println("You journey down the hallway.");
+                
             }
         }
     }
