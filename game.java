6aca138f38b419a8b6237bd27e787cf62e3de439
Perstone <prestondaily1@gmail.com>
2017-01-31 21:51:59 -0600

    Updated
    -User interface
    -Stats system
    -Method calling

--- game.java
+++ game.java
@@ -31,7 +31,14 @@ public class game
         }    
         //new page break-----------------------
             
-        System.out.println("Loading...");
+        System.out.print("loading");
+        wait(340);
+        System.out.print(".");
+        wait(340);
+        System.out.print(".");
+        wait(340);
+        System.out.print(".");
+  
         System.out.println("");
         wait(2000); 
         System.out.println("3");
@@ -41,7 +48,7 @@ public class game
         System.out.println("1");
         wait(1000);  
         System.out.println("(Gotta Blast!)");
-        wait(1000); 
+        wait(1250); 
             
         //new page break-----------------------
         for (int i = 0; i <= 40; i++){
@@ -61,11 +68,12 @@ public class game
         System.out.println("#######################################################");
         System.out.println("=======================================================");
         System.out.println("\n");
-        wait(3000);
         System.out.println("-------------------------------------------------------");
-        System.out.println("To check your stats at any time, use the following command.");
-        System.out.println("-    '/stats'");
-        
+        System.out.println("");
+        System.out.println("To check your current game stats such as health,");
+        System.out.println("armour, and inventory, use the following command.");
+        System.out.println("-  '/stats'");
+        System.out.println("");
         System.out.println("-------------------------------------------------------");
         System.out.println("");
         System.out.println("\n");
@@ -77,26 +85,43 @@ public class game
         System.out.println("");
         
         
-        System.out.println("Storing...");
+        System.out.print("Storing");
+        wait(400);
+        System.out.print(".");
+        wait(400);
+        System.out.print(".");
+        wait(400);
+        System.out.print(".");
+        wait(250);
+        System.out.println("");
+        
+        System.out.println("Your name is now set as " + user_name);
+        
         wait(1500);
         
-        System.out.println("Your name is " + user_name);
-        
-        
         System.out.println("");
         System.out.println("");
+        
+        
         
         System.out.println("Type your gender: ");
         String user_gender = user_input.next();
         System.out.println("");
         
         
-        System.out.println("Storing...");
-        wait(1500);
+        System.out.print("Storing");
+        wait(400);
+        System.out.print(".");
+        wait(400);
+        System.out.print(".");
+        wait(400);
+        System.out.print(".");
+        wait(250);
+        System.out.println("");
         
-        System.out.println("Your gender is " + user_gender);
+        System.out.println("Your gender is now set as a(n) " + user_gender);
         
-        wait(1500);
+        wait(2100);
         
         
         
@@ -110,14 +135,15 @@ public class game
         System.out.println("your thoughts, you panic in");
         System.out.println("the realization that you cannot");
         System.out.println("remember the past.");
-        wait(1000);
-        System.out.println("Finding yourself in a cold,");
+        wait(6000);
+        System.out.println("");
+        System.out.println("    Finding yourself in a cold,");
         System.out.println("dark, and rather damp corridor.");
         System.out.println("The only visible light, permiates from");
         System.out.println("a door several paces away.");
         System.out.println("You notice a crate next to you.");
         System.out.println("It is emitting a strange sound.");
-        wait(1500);
+        wait(7200);
         System.out.println("");
         System.out.println("");
         System.out.println("What do you do?");
@@ -141,8 +167,15 @@ public class game
         {
             if (s.toLowerCase().contains(lowerCheck))
             {
+                int health = 100;
+                
                 System.out.println("");
-                System.out.println("Your health: 100/100");
+                System.out.println("----------------Health Information----------------");
+                System.out.println("");
+                System.out.println("Your health: ");
+                System.out.print(health + "/100");
+                System.out.println("");
+                System.out.println("--------------------------------------------------");
             }
         }
         
@@ -151,12 +184,19 @@ public class game
         // Armour check
         
         String[] armourCheck = {"/armour"};
-        for (String s: healthCheck)
+        for (String s: armourCheck)
         {
             if (s.toLowerCase().contains(lowerCheck))
             {
+                int armour = 10;
+                
                 System.out.println("");
-                System.out.println("Your Armour rating: 10/10");
+                System.out.println("----------------Armour Information----------------");
+                System.out.println("");
+                System.out.println("Your Armour rating: ");
+                System.out.print(armour + "/10");
+                System.out.println("");
+                System.out.println("--------------------------------------------------");
             }
         }
         
@@ -164,15 +204,32 @@ public class game
         
         // Inventory check
         
-        String[] inventory = {"'Pencil'", "'necklace'"};
+        
+        
         
         String[] inventoryCheck = {"/inventory"};
-        for (String s: healthCheck)
+        for (String s: inventoryCheck)
         {
             if (s.toLowerCase().contains(lowerCheck))
             {
                 System.out.println("");
-                System.out.println("Your inventory: " + inventory);
+                System.out.println("-------------Your Inventory Includes-------------");
+                System.out.println("");
+                
+                // ==================Inventory Print==================
+                String[] inventory = {"Pencil, ", "Necklace, "}; 
+                int itemAmount = 2;
+    
+                for (int i = 0; i <= (itemAmount - 1); i++){
+        
+                    System.out.println(inventory[i]);
+        
+                }
+                // =======================End=========================
+                
+                System.out.println("");
+                System.out.println("-------------------------------------------------");
+                System.out.println("");
             }
         }
         
